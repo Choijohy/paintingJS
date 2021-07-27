1df591e54bf8d01e3c3fb883de9b8130c91660f6
Choijohy <hi5663139@naver.com>
2021-07-27 16:29:00 +0900

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const colors = document.getElementsByClassName("jsColors");
 const modes = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
 
+const recBtn = document.getElementById("jsRec");
+const rectDragBtn = document.getElementById("jsRectDrag");
+
+
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
@@ -20,32 +24,48 @@ ctx.lineWidth = 2.5;
 
 let painting = false;
 let filling = false;
+let drag = false;
+let dragStart = false;
 
 function onMouseMove(event){
     var x = event.offsetX;
     var y = event.offsetY;
    
-    if (!painting){
-        ctx.beginPath();
-        ctx.moveTo(x,y)}
+    if (dragStart){
+        canvasDraw(x,y);
+        
+    }
     else{
-        ctx.lineTo(x,y);
-        if (!filling){
-            ctx.stroke();
-            console.log("painting");
+        if (!painting){
+            ctx.beginPath();
+            ctx.moveTo(x,y);
+        }
+        else{
+            ctx.lineTo(x,y);
+            if (!filling){
+                ctx.stroke();
+                console.log("painting");
+            }
         }
     }
 }
 function startPainting(event){
-    painting = true;
-    if (filling){
+    if (drag){
+        startX = event.offsetX;
+        startY = event.offsetY;
+        dragStart =true;
+    }
+    else {
+        painting = true;
+        if (filling){
         // x,y - start point , width , height 
-        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+            ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+        }
     }
 }
-
 function stopPainting(event) {
     painting = false;
+    dragStart = false;
 }
 
 function handleColorClick(event){
@@ -81,6 +101,30 @@ function handelCM(event){
     event.preventDefault();
 }
 
+function makeRect(event){
+    var recX = document.getElementById("recX").value;
+    var recY = document.getElementById("recY").value;
+    var recWidth = document.getElementById("recWidth").value;
+    var recHeight = document.getElementById("recHeight").value;
+    ctx.fillRect(recX,recY,recWidth,recHeight);
+}
+
+function dragRect(event) {
+    if (drag === false){
+        drag = true;
+        event.target.innerText ="STOP";
+    }else{
+        drag = false;
+        event.target.innerText = "DRAG";
+    }
+}
+
+function canvasDraw(currentX,currentY)
+{
+ ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height) //설정된 영역만큼 캔버스에서 지움
+ ctx.strokeRect(startX,startY,currentX-startX,currentY-startY) //시작점과 끝점의 좌표 정보로 사각형을 그려준다.
+}
+
 if (canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mouseleave",stopPainting);
@@ -105,4 +149,12 @@ if (modes){
 
 if (saveBtn){
     saveBtn.addEventListener("click",handleSave);
+}
+
+if (recBtn){
+    recBtn.addEventListener("click",makeRect);
+}
+
+if (jsRectDrag){
+    jsRectDrag.addEventListener("click",dragRect);
 }
